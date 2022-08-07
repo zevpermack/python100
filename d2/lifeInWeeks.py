153a2c3963b746e4d18e24f413697838cc7c3333
rewpt <zpermack@gmail.com>
2022-08-06 22:37:15 -0400

    removed unneccesary type coersion

--- d2/lifeInWeeks.py
+++ d2/lifeInWeeks.py
@@ -5,4 +5,4 @@ weeks_left = 90 * 52 - age * 52
 months_left = 90 * 12 - age * 12
 
 
-print(f"you have {str(days_left)} days {str(weeks_left)} weeks and {str(months_left)} months left to live ")
+print(f"you have {days_left} days {weeks_left} weeks and {months_left} months left to live ")
