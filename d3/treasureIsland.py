4edca4bf5304dec80faca1e646f78ddf650c8740
rewpt <zpermack@gmail.com>
2022-08-07 13:13:00 -0400

    add case checking

--- d3/treasureIsland.py
+++ d3/treasureIsland.py
@@ -1,14 +1,14 @@
 print("Welcome to Treasure Island. Your mission is to find the treasure")
-q1 = input("L or R? ")
-if q1 != "L":
+q1 = input("L or R? ").lower()
+if q1 != "l":
   print("Game Over")
 else:
-  q2 = input("Swim or Wait? ")
-  if q2 != "Wait":
+  q2 = input("Swim or Wait? ").lower()
+  if q2 != "wait":
     print("Game Over")
   else:
-    q3 = input("Which door, Red, Blue or Yellow? ")
-    if q3 != "Yellow":
+    q3 = input("Which door, Red, Blue or Yellow? ").lower()
+    if q3 != "yellow":
       print("Game Over")
     else: 
       print("You Win!")
