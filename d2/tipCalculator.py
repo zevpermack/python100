52d87bc714c81cc5750b0dfc2411d1e7a7b99066
rewpt <zpermack@gmail.com>
2022-08-06 22:50:50 -0400

    use formatting instead of round to solve issue of no zero at the end when using round function

--- d2/tipCalculator.py
+++ d2/tipCalculator.py
@@ -4,7 +4,7 @@ total = float(input("What is the total bill? \n"))
 tip_percentage = float(input("What percent would you like to tip? \n"))
 people = int(input("How many people are splitting the bill? \n"))
 
-amount_per_person = round((total + (total * tip_percentage/100)) / people, 2)
+amount_per_person = "{:.2f}".format((total + (total * tip_percentage/100)) / people)
 
 final_msg = f"Each person owes ${amount_per_person}"
 print(final_msg)
