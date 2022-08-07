4a3b02fbf63b935879209d3d36271deb4a9f9ed2
rewpt <zpermack@gmail.com>
2022-08-06 22:06:24 -0400

    input cursor function to next line

--- d1/bandNameGen.py
+++ d1/bandNameGen.py
@@ -1,6 +1,6 @@
 print("--- Welcome to band name generator ---")
 
-city = input("What city did you grow up in? ")
-pet = input("What is the name of a pet you have had? ")
+city = input("What city did you grow up in?\n ")
+pet = input("What is the name of a pet you have had?\n ")
 
 print("Your band name is " + city + " " + pet)
