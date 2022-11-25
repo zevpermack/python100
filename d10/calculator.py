11c3a031911410e41fe2ce2e9def1b10a5de53ba
rewpt <zpermack@gmail.com>
2022-11-25 13:21:38 -0500

    finish calculator

--- d10/calculator.py
+++ d10/calculator.py
@@ -18,11 +18,42 @@ operations = {
 }
 
 num1 = int(input("What's the first number?: "))
-num2 = int(input("What's the second number?: "))
 
-for operation in operations:
-  print(operation)
+def get_operator():
+  for operation in operations:
+    print(operation)
 
-symbol = input("Pick an operation symbol: ")
-answer = num1 
-print(operations[symbol](num1, num2))
+  return input("Pick an operation symbol from the list above: ")
+
+def get_second_number():
+  return int(input("What's the second number?: "))
+
+def finish_equation():
+  symbol = get_operator()
+  num2 = get_second_number()
+  return symbol, num2
+
+symbol, num2 = finish_equation()
+
+def calculate(symbol, num1, num2):
+  return operations[symbol](num1, num2)
+
+curr_result = calculate(symbol, num1, num2)
+print(curr_result)
+
+def continue_calculating(previous_result): 
+  user_answer = input(f"Would you like to continue calculating on your previous result {previous_result}? 'y' for yes. 'n' for no: ")
+  if user_answer == "y":
+    return True
+  return False
+
+switch = continue_calculating(curr_result)
+while switch:
+  symbol, num2 = finish_equation()
+  curr_result = calculate(symbol, curr_result, num2)
+  print(curr_result)
+  switch = continue_calculating(curr_result)
+
+print("See ya!")
+
+
