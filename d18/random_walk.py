5a1bd6678ff24f7d2d25bf7da0ed60415ed1094b
rewpt <zpermack@gmail.com>
2023-08-06 12:14:36 -0400

    add rgb generator

---
+++ d18/random_walk.py
@@ -0,0 +1,30 @@
+import random
+import turtle
+
+turtle.colormode(255)
+
+
+rotate_angles = [0, 30, 60, 90, 120, 150, 180, 210, 240, 270]
+
+z = turtle.Turtle()
+z.pensize(20)
+
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return (r, g, b)
+
+
+def random_walk(distance):
+    for _ in range(distance):
+        z.right(rotate_angles[random.randint(0, len(rotate_angles) - 1)])
+        z.color(random_color())
+        z.forward(50)
+
+
+random_walk(100)
+
+screen = turtle.Screen()
+screen.turtle.exitonclick()
