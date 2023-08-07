7d01049cc514daa14fa9522315d259f9aa82fd0c
rewpt <zpermack@gmail.com>
2023-08-07 13:34:32 -0400

    complete etcha sketch

--- d19/etch_a_sketch.py
+++ d19/etch_a_sketch.py
@@ -2,28 +2,36 @@ import turtle
 
 screen = turtle.Screen()
 
+z = turtle.Turtle()
+
 
 def move_forward():
-    turtle.forward(10)
+    z.forward(10)
 
 
 def move_backward():
-    turtle.backward(10)
+    z.backward(10)
 
 
 def turn_left():
-    turtle.left(10)
+    z.left(10)
 
 
 def turn_right():
-    turtle.right(10)
+    z.right(10)
 
 
-screen.listen()
+def clear():
+    z.clear()
+    z.penup()
+    z.home()
+
+
 screen.onkey(key="w", fun=move_forward)
 screen.onkey(key="a", fun=turn_left)
 screen.onkey(key="s", fun=move_backward)
 screen.onkey(key="d", fun=turn_right)
-
+screen.onkey(key="c", fun=clear)
+screen.listen()
 
 screen.exitonclick()
