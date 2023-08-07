ca69a03d5625a16feb722386a780c06cd65e6c8c
rewpt <zpermack@gmail.com>
2023-08-07 12:29:13 -0400

    create etchasketch basics functions

---
+++ d19/etch_a_sketch.py
@@ -0,0 +1,29 @@
+import turtle
+
+screen = turtle.Screen()
+
+
+def move_forward():
+    turtle.forward(10)
+
+
+def move_backward():
+    turtle.backward(10)
+
+
+def turn_left():
+    turtle.left(10)
+
+
+def turn_right():
+    turtle.right(10)
+
+
+screen.listen()
+screen.onkey(key="w", fun=move_forward)
+screen.onkey(key="a", fun=turn_left)
+screen.onkey(key="s", fun=move_backward)
+screen.onkey(key="d", fun=turn_right)
+
+
+screen.exitonclick()
